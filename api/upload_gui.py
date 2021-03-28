14ff92bad2f91879a2b7cdabab333b63be3d6bfb
ktopolov <ktopolov@umich.edu>
2021-03-28 18:30:00 -0400

    upload_gui.py: added title and made font bigger

--- api/upload_gui.py
+++ api/upload_gui.py
@@ -49,45 +49,56 @@ def upload():
 window = tk.Tk()
 window.geometry("1400x500")
 
+# -- Title
+tk.Label(text='Data Collection Uploader', font=("Arial", 25)).grid(
+    row=0, column=0, rowspan=1, columnspan=2)
+long_entry = tk.Entry(font=("Arial", 16))
+long_entry.grid(row=2, column=1)
+long_entry.insert(0, '40.12')
+
 # -- Latitude
-tk.Label(text='Latitude').grid(row=0, column=0)
-lat_entry = tk.Entry()
-lat_entry.grid(row=0, column=1)
+tk.Label(text='Latitude', font=("Arial", 16)).grid(row=1, column=0)
+lat_entry = tk.Entry(font=("Arial", 16))
+lat_entry.grid(row=1, column=1)
 lat_entry.insert(0, '41.53')
 
 # -- Longitude
-tk.Label(text='Longitude').grid(row=1, column=0)
-long_entry = tk.Entry()
-long_entry.grid(row=1, column=1)
+tk.Label(text='Longitude', font=("Arial", 16)).grid(row=2, column=0)
+long_entry = tk.Entry(font=("Arial", 16))
+long_entry.grid(row=2, column=1)
 long_entry.insert(0, '40.12')
 
 # -- File Entry
 browse_button = tk.Button(
     text="Browse",
     bd=3,
-    height=3,
+    height=2,
     width=10,
     bg='yellow',
     fg='black',
-    command=get_filename).grid(row=2, column=0)
+    font=("Arial", 16),
+    command=get_filename).grid(row=3, column=0)
 
 file_str = tk.StringVar()
 file_str.set('')
-file_entry = tk.Entry(textvariable=file_str)
+file_entry = tk.Entry(textvariable=file_str,
+                      font=("Arial", 16))
 file_entry.grid(row=2, column=1)
 
 # -- Upload
 upload_button = tk.Button(
     text="Upload",
     bd=3,
-    height=3,
+    height=2,
     width=10,
     bg='green',
     fg='white',
-    command=upload).grid(row=3, column=0)
+    font=("Arial", 16),
+    command=upload).grid(row=4, column=0)
 status_str = tk.StringVar()
 status_str.set('')
-status_label = tk.Label(textvariable=status_str)
+status_label = tk.Label(textvariable=status_str,
+                        font=("Arial", 16))
 status_label.grid(row=3, column=1)
 
 tk.mainloop()
