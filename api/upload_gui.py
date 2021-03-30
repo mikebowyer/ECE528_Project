1b989f02f2ed139be7b2477da41196ccdcb3e613
ktopolov <ktopolov@umich.edu>
2021-03-30 00:46:46 -0400

    added url printout

--- api/upload_gui.py
+++ api/upload_gui.py
@@ -16,12 +16,27 @@ stage_url = 'https://dy0duracgd.execute-api.us-east-1.amazonaws.com/dev'
 
 # %% FUNCTIONS
 def get_filename():
+    """
+    Get name of file using filedialog browser
+
+    Paremters
+    ---------
+
+    Returns
+    -------
+    """
     filename = filedialog.askopenfilename()
     file_str.set(filename)
 
 def upload():
     """
-    
+    Upload a local image with metadata using API
+
+    Parameters
+    ----------
+
+    Returns
+    -------
     """
     status_str.set('UPLOADING')
     window.update()
@@ -39,9 +54,11 @@ def upload():
     request_url = stage_url + '/share-image'
     http_body_str = json.dumps(http_body)  # Must make string for put request
     share_image_response = requests.put(url=request_url, data=http_body_str).json()
+    dynamo_meta = json.loads(share_image_response['dynamoMeta'])
 
     time.sleep(1.0)
-    status_str.set(share_image_response['message'])
+    status_str.set('Uploaded to {} and \n{}'.format(
+        dynamo_meta['ImageURL'], dynamo_meta['LabeledImageURL']))
     window.update()
 
 
@@ -52,21 +69,16 @@ window.geometry("1400x500")
 # -- Title
 tk.Label(text='Data Collection Uploader', font=("Arial", 25)).grid(
     row=0, column=0, rowspan=1, columnspan=2)
-long_entry = tk.Entry(font=("Arial", 16))
-long_entry.grid(row=2, column=1)
-long_entry.insert(0, '40.12')
 
 # -- Latitude
 tk.Label(text='Latitude', font=("Arial", 16)).grid(row=1, column=0)
 lat_entry = tk.Entry(font=("Arial", 16))
 lat_entry.grid(row=1, column=1)
-lat_entry.insert(0, '41.53')
 
 # -- Longitude
 tk.Label(text='Longitude', font=("Arial", 16)).grid(row=2, column=0)
 long_entry = tk.Entry(font=("Arial", 16))
 long_entry.grid(row=2, column=1)
-long_entry.insert(0, '40.12')
 
 # -- File Entry
 browse_button = tk.Button(
@@ -82,8 +94,9 @@ browse_button = tk.Button(
 file_str = tk.StringVar()
 file_str.set('')
 file_entry = tk.Entry(textvariable=file_str,
-                      font=("Arial", 16))
-file_entry.grid(row=2, column=1)
+                      font=("Arial", 16),
+                      width=20)
+file_entry.grid(row=3, column=1)
 
 # -- Upload
 upload_button = tk.Button(
@@ -99,6 +112,6 @@ status_str = tk.StringVar()
 status_str.set('')
 status_label = tk.Label(textvariable=status_str,
                         font=("Arial", 16))
-status_label.grid(row=3, column=1)
+status_label.grid(row=4, column=1)
 
 tk.mainloop()
