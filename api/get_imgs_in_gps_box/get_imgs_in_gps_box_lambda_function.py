eabdda5bc87e1d4197750b07abb3f428db172c39
Mike Bowyer <mbowyer@umich.edum>
2021-04-04 10:14:02 -0400

    Actually pushing the working lambda function

--- api/get_imgs_in_gps_box/get_imgs_in_gps_box_lambda_function.py
+++ api/get_imgs_in_gps_box/get_imgs_in_gps_box_lambda_function.py
@@ -1,18 +1,22 @@
 import json
-
+from table_manager.dashcam_table_manager import DashcamTableManager
 
 def lambda_handler(event, context):
     try:
-        tl_lat = event['TL_Lat']
-        tl_long = event['TL_Long']
-        br_lat = event['BR_Lat']
-        br_long = event['BR_Long']
+        tl_lat = float(event['TL_Lat'])
+        tl_long = float(event['TL_Long'])
+        br_lat = float(event['BR_Lat'])
+        br_long = float(event['BR_Long'])
     except:
-        response = {'statusCode': 400,
-                    'message': 'RecievedMessage: {}'.format(event), 'body': 'Base request'}
+        response = {'statusCode': 400, 
+        'message': 'RecievedMessage: {}'.format(event),'body': 'Base request'}
+        
+    table_manager = DashcamTableManager("dashcam_images")
+    results = table_manager.get_imgs_in_GPS_bounds(tl_lat, tl_long, br_lat,
+                                                              br_long)
 
-    print(event['BR_Lat'])
-    response = {'statusCode': 200,
-                'message': 'RecievedMessage: {}'.format(event), 'body': json.dumps(event)}
-
+    response = {'statusCode': 200, 
+        'message': 'RecievedMessage: {}'.format(event),'body': results}
+                
     return response
+     
